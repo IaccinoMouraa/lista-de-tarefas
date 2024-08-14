7dcef1208b8af5a951dc5b0320aeb89914e1a06a
IaccinoMoura <jmouraiaccino@gmail.com>
2024-08-14 14:12:05 +0100

    salvando tarefas no armazenamento local

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 
-let tarefas = [];
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
 
 
 function renderTarefas(){
@@ -13,13 +13,26 @@ function renderTarefas(){
         let liElement = document.createElement("li");
         let tarefaText = document.createTextNode(todo);
 
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+
+        let linkText = document.createTextNode("Excluir");
+        linkElement.appendChild(linkText);
+
+        let posicao = tarefas.indexOf(todo);
+
+        linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`)
+
         liElement.appendChild(tarefaText);
+        liElement.appendChild(linkElement);
         listElement.appendChild(liElement);
 
 
         
     })
 }
+
+renderTarefas();
 
 function adicionarTarefas(){
     if(inputElement.value === ''){
@@ -32,8 +45,20 @@ function adicionarTarefas(){
         inputElement.value = '';
 
         renderTarefas();
+        salvarDados();
+
 
     }
 }
 
 buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao){
+    tarefas.splice(posicao, 1);
+    renderTarefas();
+    salvarDados();
+}
+
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas) )
+}
